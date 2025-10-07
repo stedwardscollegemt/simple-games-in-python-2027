bfece6932f4fb38cf7c7d841254d9ffecd3c1ab8
erikacamilleri <erika.camilleri@gmail.com>
2025-10-07 18:08:30 +0200

    Class work examples and annotated slide notes

---
+++ ch01_getting_started/basics.py
@@ -0,0 +1,6 @@
+# This is comment in Python - they are useful notes to ourselves as we learn
+
+# A simple input statement
+age = input("Enter your age:")
+# A simple output statement
+print("You are", age, "years old")
--- ch01_getting_started/turtle_maze.py
+++ ch01_getting_started/turtle_maze.py
@@ -1,16 +1,27 @@
 from turtle import *
 
-# enter maze
-forward(25)
+# some functions need a turtle object
+t = Turtle() # create turtle
+t.shape("turtle") # make it look like a turtle
+t.showturtle() # make it visible on screen
+t.color("green")
+
+# drawing a square path for my turtle called t
 
 # up
-left(90)
-forward(50)
+t.left(90)
+t.forward(50)
 
 # right
-right(90)
-forward(25)
+t.right(90)
+t.forward(50)
 
-# todo: other instructions to guide the turtle to the apple
+# down
+t.left(270)
+t.forward(50)
+
+# left
+t.left(-90)
+t.forward(50)
 
 quit = input("Press any key to quit...")
