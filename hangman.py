d26992266ab609727d566945db6e840ffe5b3256
erikacamilleri <erika.camilleri@gmail.com>
2025-10-24 09:32:11 +0200

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -13,5 +13,20 @@ def title():
 
 title()
 
-secret_word = "secret"
+secret_word = "secret" # this is a string variable
+user_hint = ["_", "_", "_", "_", "_"] # this is a list variable
+user_guesses = 0
+max_guesses = 8
+
+print("Let us play Hangman. You need to guess the word I am thinking of.")
+print("Hint: The word has", len(secret_word), "letters")
+print(user_hint)
+
+user_guess = input("Guess a letter or the word:")
+count = secret_word.count(user_guess)
+print("Did you guess a letter in the word?", count)
+
+
+
+
 
