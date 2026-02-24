7fc9588ba291aa728c05790c0a9d0c6befe5aa39
erikacamilleri <erika.camilleri@gmail.com>
2026-02-24 08:38:32 +0100

    Add files via upload

---
+++ sba/guess_animal/sba1_guess_the_animal.py
@@ -0,0 +1,48 @@
+# ------ import modules section -----------------------------------------
+import random
+
+# ------ algorithm steps section ----------------------------------------
+
+# todo: 1a.	Display the title, welcome message, instructions and options 
+#           as depicted in the example output provided.
+
+
+# todo: 1b. Create a list of animals: cat, dog, elephant, rabbit and lion.
+
+ 
+secret = random.choice(animals_list)
+
+# todo: 1c. Use variables to store: the number of turns used, the number of guesses used, and whether the user guessed. 
+#           You should initialize the number of turns used and guesses used to 0, 
+#           and the “user guessed” variable to False.
+
+
+# todo: 2a. Construct an infinite loop for the game to run.
+
+
+
+# todo: 2b. Write a condition statement that checks whether the game is over 
+#           and breaks out of the infinite loop. The game is over if the player guesses correctly 
+#           *or* uses up all the guesses *or* runs out of turns.
+
+
+
+# todo: 2c. During game play ask the user to enter their choice.
+
+
+
+
+# todo: 3a. When the player chooses a question (options A – D), 
+#           display a “Yes” or “No” answer depending on the secret animal.
+
+
+
+# todo: 3b. When the player chooses to guess (option E), prompt the user to input an answer.
+
+
+
+# todo: 3c. Update the number of turns and guesses as appropriate.
+
+
+
+# todo: 3d. Use a Boolean expression to record whether the user has guessed the secret animal.
